883e5a3362477fbaf248cac5e942f11a0ae0afbf
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-17 23:22:17 +0800

    史诗：Jce 写入器支持写入主要类型

---
+++ internal/pkg/jce/struct.go
@@ -0,0 +1,19 @@
+package jce
+
+const (
+	Byte    = 0
+	Bool    = 0
+	Int16   = 1
+	Int32   = 2
+	Int64   = 3
+	Float32 = 4
+	Float64 = 5
+	String  = 6
+	String2 = 7
+	Map     = 8
+	Slice   = 9
+	Begin   = 10
+	End     = 11
+	Zero    = 12
+	Bytes   = 13
+)
--- internal/pkg/jce/writer.go
+++ internal/pkg/jce/writer.go
@@ -1,19 +1,93 @@
 package jce
 
-import "bytes"
+import (
+	"bytes"
+	"encoding/binary"
+)
 
 // writer 写入器
-type writer struct{ b *bytes.Buffer }
+type writer struct {
+	b   *bytes.Buffer // 缓冲区
+	tag uint8         // 标签
+}
 
 // NewWriter 返回一个写入器
-func NewWriter() *writer { return &writer{bytes.NewBuffer(nil)} }
+func NewWriter(tag uint8) *writer { return &writer{bytes.NewBuffer(nil), tag} }
 
 // writeKey 写入键
-func (w *writer) writeKey(Type byte, tag uint8) {
-	if tag > 14 {
+func (w *writer) writeKey(Type byte) {
+	if w.tag > 14 {
 		w.b.WriteByte(Type | 0xF0)
-		w.b.WriteByte(tag)
-		return
+		w.b.WriteByte(w.tag)
+	} else {
+		w.b.WriteByte(w.tag<<4 | Type)
 	}
-	w.b.WriteByte(tag<<4 | Type)
+	w.tag++
+}
+
+// WriteByte 写入 Byte
+func (w *writer) WriteByte(b byte) *writer {
+	w.writeKey(Byte)
+	w.b.WriteByte(b)
+	return w
+}
+
+// WriteBool 写入 Bool
+func (w *writer) WriteBool(b bool) *writer {
+	w.writeKey(Bool)
+	var by byte = 0
+	if b {
+		by = 1
+	}
+	w.b.WriteByte(by)
+	return w
+}
+
+// WriteInt16 写入 Int16
+func (w *writer) WriteInt16(i int16) *writer {
+	w.writeKey(Int16)
+	_ = binary.Write(w.b, binary.BigEndian, i)
+	return w
+}
+
+// WriteInt32 写入 Int32
+func (w *writer) WriteInt32(i int32) *writer {
+	w.writeKey(Int32)
+	_ = binary.Write(w.b, binary.BigEndian, i)
+	return w
+}
+
+// WriteInt64 写入 Int64
+func (w *writer) WriteInt64(i int64) *writer {
+	w.writeKey(Int64)
+	_ = binary.Write(w.b, binary.BigEndian, i)
+	return w
+}
+
+// WriteFloat32 写入 Float32
+func (w *writer) WriteFloat32(i float32) *writer {
+	w.writeKey(Float32)
+	_ = binary.Write(w.b, binary.BigEndian, i)
+	return w
+}
+
+// WriteFloat64 写入 Float64
+func (w *writer) WriteFloat64(i float64) *writer {
+	w.writeKey(Float64)
+	_ = binary.Write(w.b, binary.BigEndian, i)
+	return w
+}
+
+// WriteString 写入 String
+func (w *writer) WriteString(s string) *writer {
+	if len(s) > 255 {
+		w.writeKey(String2)
+		_ = binary.Write(w.b, binary.BigEndian, len(s))
+		w.b.WriteString(s)
+		return w
+	}
+	w.writeKey(String)
+	w.b.WriteByte(byte(len(s)))
+	w.b.WriteString(s)
+	return w
 }
